4882e441e2219d0eef1dd7e8dcdff584a7b78613
FerPM <46204965+codigorojo@users.noreply.github.com>
2021-03-19 16:43:07 -0300

    al pasar el try/catch para el controller no funca

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-//En app.js queda la lógica de ruteo.
-
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const librosControlers = require("./controllers/librosControler");
 
 app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT ? process.env.PORT : 3000;
 
-const librosControlers = require("./controllers/librosControler");
-
-app.get("/libro/:id", async function (req, res) {
-  try {
-    let respuesta = await librosControlers.traerUnLibro(req.params.id);
-    if (respuesta.length === 1) {
-      res.send(respuesta[0]);
-    } else {
-      res.status(404);
-      throw new Error("No se encuentra ese libro");
-    }
-  } catch (e) {
-    res.status(413).send({ Error: e.message });
-  }
-});
+app.use("/libro/:id", librosControlers);
 
 // Se ejecuta la app para que escuche al puerto determinado
 app.listen(PORT, () => {
--- controllers/librosControler.js
+++ controllers/librosControler.js
@@ -1,10 +1,19 @@
-//El controller se encarga de verificar la información que será enviada a los services. Verifica que los datos obtenidos cumplan con la lógica de negocios.
-
+const express = require("express");
 const librosService = require("../services/librosService");
+const app = express.Router();
 
-module.exports = {
-  traerUnLibro: async function (id) {
-    let libro = await librosService.libroGet(id);
-    return libro;
-  },
-};
+app.get("/:id", async (req, res) => {
+  try {
+    let respuesta = await librosService.traerUnLibro(req.params.id);
+    if (respuesta.length === 1) {
+      res.send(respuesta[0]);
+    } else {
+      res.status(404);
+      throw new Error("No se encuentra ese libro");
+    }
+  } catch (e) {
+    res.status(413).send({ Error: e.message });
+  }
+});
+
+module.exports = app;
